450bbf160ecca72543a883195f4d26a4168d6e63
guptaco@ymail.com <guptaco@ymail.com>
2018-03-02 01:56:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function destructivelyRemoveLastKitten(){
   kittens.pop("Garfield")
 }
 
-function destructivelyRemoteFirstKitten(){
+function destructivelyRemoveFirstKitten(){
   kittens.shift("Bob")
 }
