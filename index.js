e603dad55a9cfdb7c943da6f82372b9f8f2bdb7c
guptaco@ymail.com <guptaco@ymail.com>
2018-03-02 01:50:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(catName){
+function destructivelyAppendKitten(){
 kittens.unshift(catName)
 }
